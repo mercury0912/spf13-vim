cadf2d847cdcb842c629e6360a160699efaccc54
Mercury <zhaodi_2006@163.com>
2016-09-03 12:19:05 +0800

    Modify to suit for my usage habit
    add 'plasticboy/vim-markdown' to Plugin list, replacing 'tpope/vim-markdown'
    Cancel spell check and backup function
    set g:vim_markdown_conceal = 0 and g:textobj#quote#educate = 0 for vim-markdown and
    vim-textobj-quote respectively.

--- bootstrap.sh
+++ bootstrap.sh
@@ -16,10 +16,10 @@
 
 ############################  SETUP PARAMETERS
 app_name='spf13-vim'
-[ -z "$APP_PATH" ] && APP_PATH="$HOME/.spf13-vim-3"
-[ -z "$REPO_URI" ] && REPO_URI='https://github.com/spf13/spf13-vim.git'
+[ -z "$APP_PATH" ] && APP_PATH="$HOME/Downloads/repo/spf13-vim"
+[ -z "$REPO_URI" ] && REPO_URI='git@github.com:mercury0912/spf13-vim.git'
 [ -z "$REPO_BRANCH" ] && REPO_BRANCH='3.0'
-debug_mode='0'
+debug_mode='0
 fork_maintainer='0'
 [ -z "$VUNDLE_URI" ] && VUNDLE_URI="https://github.com/gmarik/vundle.git"
 
